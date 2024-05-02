60b23fb58291c00e6fb15ba6f7f66ab4047273f5
BhushanDharane <156442041+BhushanDharane@users.noreply.github.com>
2024-05-03 00:41:51 +0530

    Add files via upload

---
+++ College_Bus_Android_Panel/Add-One-Time-Fee.aspx.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Add_One_Time_Fee : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connection"].ConnectionString);
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        LblId.Visible = true;
+        if (Session["id"] != null)
+        {
+            LblId.Text = Session["id"].ToString();
+
+            if (LblId.Text != null)
+            {
+                Binddata(LblId.Text);
+            }
+            Session.RemoveAll();
+        }
+    }
+
+    private void Binddata(string Id)
+    {
+        try
+        {
+            btnsubmit.Text = "Update";
+            SqlCommand com;
+            string str;
+
+            con.Open();
+            str = "select * from one_time_fee where id='" + LblId.Text.Trim() + "'";
+            com = new SqlCommand(str, con);
+            SqlDataReader rd = com.ExecuteReader();
+            if (rd.Read())
+            {
+                txtstop_name.Text = rd["stop_name"].ToString();
+                txtfee.Text = rd["fee"].ToString();
+                txtstud_name.Text = rd["stud_name"].ToString();
+                txtbus_no.Text = rd["bus_no"].ToString();
+                txttime.Text = rd["bus_time"].ToString();
+                txtdate.Text = rd["date"].ToString();
+            }
+        }
+        catch (Exception e1)
+        {
+            //string message = e1.Message;
+        }
+        finally
+        {
+            con.Close();
+        }
+    }
+
+    protected void btnsubmit_Click(object sender, EventArgs e)
+    {
+        if (btnsubmit.Text == "Update")
+        {
+            Update_data();
+        }
+        else
+        {
+            Insert_data();
+        }
+    }
+
+    private void Update_data()
+    {
+        try
+        {
+            con.Open();
+            string query = String.Format("update one_time_fee set stop_name=@stop_name, fee=@fee, stud_name=@stud_name, bus_no=@bus_no, bus_time=@bus_time, date=@date where id='{0}'", LblId.Text);
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            cmd.Parameters.AddWithValue("@stop_name", txtstop_name.Text);
+            cmd.Parameters.AddWithValue("@fee", txtfee.Text);
+            cmd.Parameters.AddWithValue("@stud_name", txtstud_name.Text);
+            cmd.Parameters.AddWithValue("@bus_no", txtbus_no.Text);
+            cmd.Parameters.AddWithValue("@bus_time", txttime.Text);
+            cmd.Parameters.AddWithValue("@date", txtdate.Text);
+
+            cmd.ExecuteNonQuery();
+        }
+        catch (Exception e1)
+        {
+            //string msg = e1.Message;
+        }
+        finally
+        {
+            con.Close();
+            Response.Redirect("Add-One-Time-Fee.aspx");
+        }
+    }
+
+    private void Insert_data()
+    {
+        try
+        {
+            con.Open();
+            string query = String.Format("insert into one_time_fee (stop_name, fee, stud_name, bus_no, bus_time, date) values (@stop_name, @fee, @stud_name, @bus_no, @bus_time, @date)");
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            cmd.Parameters.AddWithValue("@stop_name", txtstop_name.Text);
+            cmd.Parameters.AddWithValue("@fee", txtfee.Text);
+            cmd.Parameters.AddWithValue("@stud_name", txtstud_name.Text);
+            cmd.Parameters.AddWithValue("@bus_no", txtbus_no.Text);
+            cmd.Parameters.AddWithValue("@bus_time", txttime.Text);
+            cmd.Parameters.AddWithValue("@date", txtdate.Text);
+
+            cmd.ExecuteNonQuery();
+        }
+        catch (Exception e1)
+        {
+            //string msg = e1.Message;
+        }
+        finally
+        {
+            con.Close();
+            Response.Redirect("Add-One-Time-Fee.aspx");
+        }
+    }
+}
---
+++ College_Bus_Android_Panel/ViewReceipt.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="ViewReceipt.aspx.cs" Inherits="ViewReceipt" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <section id="main-content">
+        <section class="wrapper">
+            <div class="row">
+                <div class="col-md-12">
+                    <div data-collapsed="0" class="panel">
+                        <div class="panel-heading">
+                            <div class="panel-title">
+                                View Receipt
+                                <asp:Label ID="LblId" runat="server" Visible="false"></asp:Label>
+                            </div>
+                        </div>
+                        <asp:Label ID="lblmobile" runat="server" Visible="false"></asp:Label>
+                        <asp:Label ID="lblname" runat="server" Visible="false"></asp:Label>
+                        <div class="panel-body">
+                            <div style="width: 1040px; height: 480px; overflow: scroll">
+                                <asp:GridView ID="GridView1" runat="server" CssClass="table table-bordered" AutoGenerateColumns="False" DataKeyNames="id" DataSourceID="SqlDataSource1">
+                                    <Columns>
+                                        <asp:BoundField DataField="id" HeaderText="id" ReadOnly="True" InsertVisible="False" SortExpression="id"></asp:BoundField>
+                                        <asp:ImageField DataImageUrlField="image">
+                                            <ControlStyle Height="100px" Width="100px" />
+                                        </asp:ImageField>
+                                        <asp:BoundField DataField="stud_name" HeaderText="stud_name" SortExpression="stud_name"></asp:BoundField>
+                                    </Columns>
+                                </asp:GridView>
+                                <asp:SqlDataSource runat="server" ID="SqlDataSource1" ConnectionString='<%$ ConnectionStrings:connection %>' SelectCommand="SELECT [id], [image], [stud_name] FROM [upload_receipt]"></asp:SqlDataSource>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </section>
+</asp:Content>
+
